d54c6237c0e319506baa4f495cf1d6a37ea0bc04
Jeremy Weinstein <jeremydw@gmail.com>
2018-06-21 16:05:21 -0700

    Work on UI.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var fs = require('fs');
 var github = require('octonode');
+var handlebars = require('handlebars');
+var bodyParser = require('body-parser');
 var matter = require('gray-matter');
 var sha1 = require('sha1');
 
@@ -18,30 +20,84 @@ var b64decode = function(content) {
 
 
 var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
-app.get('/', function (req, res) {
+app.post('/:owner/:name/tree/:branch/:filePath(*)', function(req, res) {
   var client = github.client(TOKEN);
+  var repoName = req.params.owner + '/' + req.params.name
+  var branch = req.params.branch;
+  var path = req.params.filePath;
+  var repo = client.repo(repoName);
 
-  var repo = client.repo('jeremydw/hhkaffee.com');
-  var path = 'content/pages/index.md';
-  var commitMessage = 'Updated via editor prototype.';
+  var commitMessage = req.body.commitMessage || 'Updated via editor prototype.';
+  var sha = req.body.sha;
+  var yamlAsText = req.body.yamlAsText;
+  var markdownAsText = req.body.markdownAsText;
+  var tempMatter = matter('---\n' + yamlAsText + '\n---\n');
+  var contentToWrite = matter.stringify(markdownAsText, tempMatter.data);
 
-  repo.contents(path, 'master', function(err, body, headers, foo) {
-    var b64content = body['content'];
-    var content = b64decode(b64content);
-    var hash = body['sha'];
-    var obj = matter(content);
+  console.log('Saving...');
+  repo.updateContents(
+      path, commitMessage, contentToWrite, sha, branch, function(err, body, headers) {
+    repo.contents(path, branch, function(err, body, headers, foo) {
 
-    // Update 'foo' with random value.
-    var value = parseInt((Math.random() * 100000));
-    obj.data['foo'] = value;
+      // TODO: Pull this out so the code isn't repeated.
+      fs.readFile('templates/app.html', 'utf-8', function(error, templateContent) {
+        var b64content = body['content'];
+        var content = b64decode(b64content);
+        var sha = body['sha'];
+        var obj = matter(content, {excerpt: true});
+        var params = {
+          repoName: repoName,
+          status: 'Saved',
+          sha: sha,
+          branch: branch,
+          dataAsText: JSON.stringify(obj.data),
+          path: path,
+          matter: obj
+        };
+        var template = handlebars.compile(templateContent);
+        var html = template(params);
+        res.send(html);
+      });
 
-    var text = matter.stringify(obj.content, obj.data);
-    var resp = repo.updateContents(
-        path, commitMessage, text, hash, 'master', function(err, body, headers) {
-      res.send('Saved!');
     });
+  });
+});
+
+
+app.get('/:owner/:name/tree/:branch/:filePath(*)', function(req, res) {
+  var client = github.client(TOKEN);
+  var repoName = req.params.owner + '/' + req.params.name
+  var branch = req.params.branch;
+  var path = req.params.filePath;
+  var repo = client.repo(repoName);
+
+  repo.contents(path, branch, function(err, body, headers, foo) {
+
+    // TODO: Pull this out so the code isn't repeated.
+    fs.readFile('templates/app.html', 'utf-8', function(error, templateContent) {
+      var b64content = body['content'];
+      var content = b64decode(b64content);
+      var sha = body['sha'];
+      // TODO: This doesn't work for YAML-only files. For YAML files disregard
+      // the frontmatter parser.
+      var obj = matter(content, {excerpt: true});
+      var params = {
+        repoName: repoName,
+        sha: sha,
+        branch: branch,
+        dataAsText: JSON.stringify(obj.data),
+        path: path,
+        matter: obj
+      };
+      var template = handlebars.compile(templateContent);
+      var html = template(params);
+      res.send(html);
+    });
+
   });
 });
 
